639a8d700e727d16390eeb0e0c822f2d21618917
Buggz Evol <56521233+lGodHatesMel@users.noreply.github.com>
2023-09-06 07:07:29 -0400

    updates to Discord Bot

--- Scripts/Bots/Discord Bot/DiscordBot.py
+++ Scripts/Bots/Discord Bot/DiscordBot.py
@@ -1,12 +1,15 @@
 import discord
 import asyncio
 import json
+from datetime import datetime, timezone
 
 intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
 
 client = discord.Client(intents=intents)
+
+countdown_message = None  # Initialize countdown message
 
 def load_config():
     with open('config.json', 'r') as config_file:
@@ -16,11 +19,36 @@ config = load_config()
 bot_token = config['token']
 command_prefix = config['command_prefix']
 target_channels = config['target_channels']
+target_timestamp = config['target_timestamp']
+countdown_channel_id = config['countdown_channel_id']
+
 
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user.name} ({client.user.id})')
     await send_messages()
+
+    global countdown_message
+    channel = client.get_channel(countdown_channel_id)
+
+    while True:
+        current_timestamp = datetime.now(timezone.utc).timestamp()
+        time_remaining = target_timestamp - current_timestamp
+
+        if time_remaining <= 0:
+            countdown_text = "DLC IS NOW OUT!!!\nCountdown has ended!"
+        else:
+            days = int(time_remaining // 86400)
+            hours = int((time_remaining % 86400) // 3600)
+            minutes = int((time_remaining % 3600) // 60)
+            countdown_text = f"POKEMON SCARLET & VIOLET DLC DROPS IN \n\n`{days} days, {hours} hours, {minutes} minutes`"
+
+        if countdown_message is None:
+            countdown_message = await channel.send(countdown_text)
+        else:
+            await countdown_message.edit(content=countdown_text)
+
+        await asyncio.sleep(60)
 
 async def send_messages():
     while True:
@@ -52,9 +80,6 @@ async def handle_command(message):
 
     if command == 'help':
         await message.channel.send('Available commands: !help, !about, !ping, !announcement')
-
-    elif command == 'about':
-        await message.channel.send('I am a Discord bot. Feel free to ask questions!')
 
     elif command == 'ping':
         await message.channel.send('Pong!')
---
+++ Scripts/Bots/Discord Bot/readme.md
@@ -0,0 +1,42 @@
+# Discord Bot Template
+
+This is a template for a Discord bot written in Python using the discord.py library. The bot comes with several features and is designed to be easily configurable.
+
+## Features
+
+- Countdown timer to a specified date and time.
+- Send messages to multiple channels at specified intervals.
+- Handle custom commands.
+
+## Prerequisites
+
+Before you can use this bot, you need to have the following:
+
+- Python 3.7 or higher installed on your system.
+- Discord bot token. You can obtain one by creating a bot on the [Discord Developer Portal](https://discord.com/developers/applications).
+
+## Setup
+
+1. Clone this repository or download the code as a ZIP file and extract it to your preferred directory.
+
+   ```bash
+   git clone https://github.com/lGodHatesMel/RandomResources.git
+
+2. Install the required Python packages by running the following command:
+
+   ```shell
+   pip install -r requirements.txt
+
+## Set up config.json
+
+For info on how to set up the config.json click here [Config Set up](ConfigSetup.md) for details
+
+## Usage
+
+1. Run the script using the following command:
+   ```bash
+   python DiscordBot.py
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
